6a06ad37a264e903e56b9add8b3484c4d95b0c42
1101 <110100010111h@gmail.com>
2019-07-08 08:39:58 -0600

    Prueba de rendimiento

--- src/client.cpp
+++ src/client.cpp
@@ -6,9 +6,11 @@
 int main(int argc, char**argv){
     Network n(1102,1);
     PacketHeader header("192.168.0.16",1101);
-    n.send(header,"Hello from client");
-    char buffer[MAX_DATA_SIZE];
-    n.receive(buffer);
-    std::cout<<buffer<<std::endl;
+    for(int i = 0; ;i++){
+        n.send(header,"Hello from client");
+        char buffer[MAX_DATA_SIZE];
+        n.receive(buffer);
+        std::cout<<i<<":"<<buffer<<std::endl;
+    }
     return EXIT_SUCCESS;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@
 
 int main(int argc, char**argv){
     Network n(1101);
-    while(true){
+    for(int i = 0; ;i++){
         char buffer[MAX_DATA_SIZE];
         PacketHeader rec = n.receive(buffer);
-        std::cout<<buffer<<std::endl;
+        std::cout<<i<<":"<<buffer<<std::endl;
         PacketHeader header(rec.from,rec.portFrom);
         n.send(header,"hello from server.");
     }
